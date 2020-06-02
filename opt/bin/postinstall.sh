3b46166e3a4886ba0edefaa0c5ac848a34f0ff91
Tony Vo <4.tonyvo@gmail.com>
2020-06-02 08:53:34 +0000

    initial commit

---
+++ opt/bin/postinstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# description: parallelcluster post-install script
+#
+###
+
+BINDIR=/data/opt/bin
+
+source ${BINDIR}/install-pkg
+source ${BINDIR}/install-user
+
+source ${BINDIR}/configure-host
